ae6cd9da39d526ac4dc4d85073748d6062dd2b26
Denise <deniselewis1120@gmail.com>
2024-02-25 14:16:39 -0800

    Create random selection

--- main.py
+++ main.py
@@ -24,5 +24,26 @@ def art_spark():
                 return render_template("home.html", category=category, difficulty=difficulty, project=random.choice(advanced_projects[category]), errors=[])
     return render_template("home.html", category="", difficulty="", errors=errors)
 
+@app.route('/random', methods=["POST"])
+def randomize():
+    # figure out how to pick a random value nested in a list within a dictionary
+    # get a random number to choose the difficulty (switch scenario)
+
+    # try random.choice()
+
+    categories = ["medium", "theme", "technique", "color-palette", "emotion", "art-movement"]
+    difficulties = ["beginner", "intermediate", "advanced"]
+
+    random_catergory = random.choice(categories)
+    random_difficulty = random.choice(difficulties)
+    random_project = 100 #random_difficulty[random_catergory]
+
+    if random_difficulty == "beginner":
+        return render_template("home.html", category=random_catergory, difficulty=random_difficulty, project=random.choice(beginner_projects[random_catergory]))
+    if random_difficulty == "intermediate":
+        return render_template("home.html", category=random_catergory, difficulty=random_difficulty, project=random.choice(intermediate_projects[random_catergory]))
+    if random_difficulty == "advanced":
+        return render_template("home.html", category=random_catergory, difficulty=random_difficulty, project=random.choice(advanced_projects[random_catergory]))
+
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
