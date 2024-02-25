e16a4577b3e6be6137fd621ba171276a825c8f1d
Denise <deniselewis1120@gmail.com>
2024-02-25 14:21:32 -0800

    Remove comments

--- main.py
+++ main.py
@@ -26,17 +26,11 @@ def art_spark():
 
 @app.route('/random', methods=["POST"])
 def randomize():
-    # figure out how to pick a random value nested in a list within a dictionary
-    # get a random number to choose the difficulty (switch scenario)
-
-    # try random.choice()
-
     categories = ["medium", "theme", "technique", "color-palette", "emotion", "art-movement"]
     difficulties = ["beginner", "intermediate", "advanced"]
 
     random_catergory = random.choice(categories)
     random_difficulty = random.choice(difficulties)
-    random_project = 100 #random_difficulty[random_catergory]
 
     if random_difficulty == "beginner":
         return render_template("home.html", category=random_catergory, difficulty=random_difficulty, project=random.choice(beginner_projects[random_catergory]))
